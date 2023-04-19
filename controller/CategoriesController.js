5638112b4cc76dc295441d1a889812b4df9f0a70
Giovanni Sena <giovanni.sena@outlook.com>
2023-04-19 17:27:04 -0300

    Update DB com ação editar

--- controller/CategoriesController.js
+++ controller/CategoriesController.js
@@ -21,7 +21,7 @@ router.post("/categories/save",(req,res) =>{
     }
 });
 
-router.get("/admin/categories",(req,res) =>{
+router.get("/admin/categories", (req,res) =>{
 
     Category.findAll().then(categories =>{
         res.render("admin/categories/index",{
@@ -30,7 +30,7 @@ router.get("/admin/categories",(req,res) =>{
     });
 });
 
-router.post("/categories/delete", (req,res) =>{
+router.post("/categories/delete",(req,res) =>{
     var id = req.body.id;
     if(id != undefined){
         if(!isNaN(id)){
@@ -49,7 +49,7 @@ router.post("/categories/delete", (req,res) =>{
     }
 });
 
-router.get("/admin/categories/edit/:id", (req, res)=>{
+router.get("/admin/categories/edit/:id",(req, res) =>{
         var id = req.params.id;
         if(isNaN(id)){
             res.redirect("/admin/categories");
@@ -67,4 +67,17 @@ router.get("/admin/categories/edit/:id", (req, res)=>{
             res.redirect("/admin/categories");
         })
 });
+
+router.post("/categories/update",(req,res) =>{
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update(
+        {title: title,slug: slugify(title)},
+        {where:{id: id}
+    }).then(() => {
+        res.redirect("/admin/categories");
+    });
+});
+
 module.exports= router;
