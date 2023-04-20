a3ff52baf67c138e6ae715e6f11c076b580c3834
Giovanni Sena <giovanni.sena@outlook.com>
2023-04-20 11:12:35 -0300

    Incluindo o TinyMCE na viewer Artigos

--- controller/ArticlesController.js
+++ controller/ArticlesController.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Category = require("../model/Category");
 
 // Routes
 router.get("/articles",(req,res) =>{
     res.send("Teste de rota");
+})
+
+router.get("/admin/articles/new",(req,res) =>{
+    res.render("admin/articles/new");
 })
 
 module.exports = router;
