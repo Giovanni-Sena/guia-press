bb2fe71c09447ba56192be95492352083a12fdf0
Giovanni Sena <giovanni.sena@outlook.com>
2023-04-26 08:04:43 -0300

    Ordenando os artigos na home

--- index.js
+++ index.js
@@ -17,12 +17,32 @@ aplication.use(express.json());
 aplication.use(express.static('public'));
 // Routes
 aplication.get("/",(req,res) =>{
-    Article.findAll().then(articles =>{
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ]
+    }).then(articles =>{
         res.render("index",{articles: articles});
     });
 });
 aplication.use("/",categoriesController);
 aplication.use("/",articlesController);
+aplication.get("/:slug",(req,res) =>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug: slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            res.render("article",{article: article});
+        }else{
+            res.redirect("/");
+        }
+    }).catch(error =>{
+        res.redirect("/");
+    })
+})
 //Server
 aplication.listen(8080, () =>{
     console.log("Servidor em execução!");
